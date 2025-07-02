4cd20be40b35e2632e3c65ff441ae0028909f302
jeong.lee <com2769@gmail.com>
2025-07-02 15:55:04 +0900

    [Refactoring] Fix PartialScore

--- SimilarityChecker/similarity_checker.cpp
+++ SimilarityChecker/similarity_checker.cpp
@@ -4,10 +4,20 @@ using std::string;
 
 class SimilarityChecker {
 public:
-	int getScore(string a, string b) {
+	double getScore(string a, string b) {
 		if (a.size() == b.size()) return 60;
-		if (a.size() == 5 && b.size() == 3) return 20;
-		if(a.size() == 2 && b.size() == 3) return 30;
-		return 0;
+
+		int longerStrNum = 0;
+		int shorterStrNum = 0;
+		if (a.size() > b.size()) {
+			longerStrNum = a.size();
+			shorterStrNum = b.size();
+		}
+		else {
+			longerStrNum = b.size();
+			shorterStrNum = a.size();
+		}
+		double gap = longerStrNum - shorterStrNum;
+		return (1 - (gap / shorterStrNum)) * 60;
 	}
 };
--- SimilarityChecker/similarity_checker_test.cpp
+++ SimilarityChecker/similarity_checker_test.cpp
@@ -4,25 +4,25 @@
 TEST(SimilarityChecker, LengthMaxScore) {
 	SimilarityChecker checker;
 
-	EXPECT_EQ(60, checker.getScore("ASD", "DSA"));
+	EXPECT_DOUBLE_EQ(60.0, checker.getScore("ASD", "DSA"));
 }
 
 TEST(SimilarityChecker, LengthZeroScore) {
 	SimilarityChecker checker;
 
-	EXPECT_EQ(0, checker.getScore("A", "BB"));
+	EXPECT_DOUBLE_EQ(0, checker.getScore("A", "BB"));
 }
 
 TEST(SimilarityChecker, LengthPartialScore20) {
 	SimilarityChecker checker;
 
-	EXPECT_EQ(20, checker.getScore("AAABB", "BAA"));
+	EXPECT_DOUBLE_EQ(20.0, checker.getScore("AAABB", "BAA"));
 }
 
 TEST(SimilarityChecker, LengthPartialScore30) {
 	SimilarityChecker checker;
 
-	EXPECT_EQ(30, checker.getScore("AA", "AAE"));
+	EXPECT_DOUBLE_EQ(30.0, checker.getScore("AA", "AAE"));
 }
 
 int main() {
