088e68aef3735b7398eef889f2dc432f2e9c68c9
jeong.lee <com2769@gmail.com>
2025-07-02 16:08:27 +0900

    [Refactoring] Fix getScore

--- SimilarityChecker/similarity_checker.cpp
+++ SimilarityChecker/similarity_checker.cpp
@@ -5,19 +5,37 @@ using std::string;
 class SimilarityChecker {
 public:
 	double getScore(string a, string b) {
-		if (a.size() == b.size()) return 60;
+		if (a.size() == b.size()) return MAX_SCORE;
+		return getLengthPartialScore(a, b);
+	}
 
+private:
+	double getLengthPartialScore(string& a, string& b)
+	{
+		std::pair<int, int> strNum = getWhichLonger(a.size(), b.size());
+		double gap = static_cast<double>(strNum.first - strNum.second);
+		return getLengthPartialFinalScore(gap, strNum.second);
+	}
+
+	std::pair<int, int> getWhichLonger(int aSize, int bSize) {
 		int longerStrNum = 0;
 		int shorterStrNum = 0;
-		if (a.size() > b.size()) {
-			longerStrNum = a.size();
-			shorterStrNum = b.size();
+
+		if (aSize > bSize) {
+			longerStrNum = aSize;
+			shorterStrNum = bSize;
 		}
 		else {
-			longerStrNum = b.size();
-			shorterStrNum = a.size();
+			longerStrNum = bSize;
+			shorterStrNum = aSize;
 		}
-		double gap = longerStrNum - shorterStrNum;
-		return (1 - (gap / shorterStrNum)) * 60;
+
+		return { longerStrNum, shorterStrNum };
 	}
+
+	double getLengthPartialFinalScore(double gap, int shorterStrNum) {
+		return (1 - static_cast<double>(gap / shorterStrNum)) * 60;
+	}
+
+	double MAX_SCORE = 60;
 };
